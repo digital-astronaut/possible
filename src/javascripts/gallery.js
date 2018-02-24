e0b89dbd35478b927ce9fb529e7b26cd5a33c37a
Łukasz Goral <lukasz.goral.design@gmail.com>
2018-02-24 11:30:36 +0100

    css transitions
    added css transitions

--- src/javascripts/gallery.js
+++ src/javascripts/gallery.js
@@ -2,7 +2,6 @@ import jQuery from "jquery";
 window.$ = window.jQuery = jQuery;
 "use strict";
 
-console.log('Gallery loaded');
 (function () {
 
   $(document).ready(
@@ -18,10 +17,10 @@ console.log('Gallery loaded');
 
 
 
-      // window.alert(galleryPhotos.length);
+
       galleryThumbnails.click(function()
       {
-        //    $('.gallery__photo').eq(activePhoto).removeClass('gallery__photo--active');
+
         galleryThumbnails.removeClass('gallery__thumbnail--active');
         galleryPhotos.removeClass('gallery__photo--active');
         activePhoto = galleryThumbnails.index(this);
